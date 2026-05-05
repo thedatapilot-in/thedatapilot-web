3dc6f5f9c4502d44543ec751d1a66b5db75b9cfc
The Data Pilot <thedatapilot.in@gmail.com>
2026-05-05 21:23:44 +0200

    Clean up unused files, backups, and unnecessary PDF

--- test_parse.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const babel = require('@babel/core');
-const code = fs.readFileSync('landing-page.js', 'utf-8');
-try {
-  babel.parse(code, { presets: ['@babel/preset-react'] });
-  console.log("No syntax errors!");
-} catch (e) {
-  console.error("Syntax Error:", e);
-}
